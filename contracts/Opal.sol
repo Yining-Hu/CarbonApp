4ed6ba62668fc3dd784f82d631c91cb3b2522a80
Yining Hu <yining.hu@uts.edu.au>
2022-04-27 09:37:02 +1000

    clean-ups

--- contracts/Opal.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
-
-import "./DigitalTwin.sol";
-
-contract Opal is DigitalTwin {
-    constructor() DigitalTwin("Opal Token", "OPAL") {}
-}
--- migrations/4_deploy_opal.js
+++
@@ -1,5 +0,0 @@
-const opal = artifacts.require('Opal');
-
-module.exports = function(deployer,network,accounts){
-    deployer.deploy(opal,{from:accounts[1]});
-};
