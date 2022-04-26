ab7636e052e93ed90a81c4f58847453e988ecdbb
Yining Hu <yining.hu@uts.edu.au>
2022-04-27 09:40:34 +1000

    removes the now ignored files

--- migrations/3_deploy_shred.js
+++
@@ -1,8 +0,0 @@
-const moz = artifacts.require('Moz');
-const shred = artifacts.require('Shred');
-
-module.exports = function(deployer){
-    deployer.deploy(moz).then(function() {
-        return deployer.deploy(shred, moz.address);
-    });
-};
