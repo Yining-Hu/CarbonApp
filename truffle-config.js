feb169deea176fc0d3cb3051a53a5bea04924a11
Yining Hu <yining.hu@uts.edu.au>
2022-10-27 15:23:14 +1100

    revises how to get provider

--- truffle-config.js
+++ truffle-config.js
@@ -1,21 +1,21 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
-// const privKeyBestonChain = ["16e04e4441a5786f29ca78f65f331fa7a99d514aa8e698d49d4263cd8403704e","9201c575623ca41a815e7c79912dd93cf438498b580968298905eefc31c79326","70468f27978d713b18564667c50fe126a5ac243c3a19fd4557d669c4195404aa"]; // validator.key of beston-chain-1
+const privKeyBestonChain = ["6c5a32ce12b866ba6d658010b90960d51ff6f9a7da1510783e872dbab11433d2","6c5a32ce12b866ba6d658010b90960d51ff6f9a7da1510783e872dbab11433d2","c796465b8a824db6ab1317cb5b238e5912a718670ac44e3fa13f71996310fa2d"]
 
 // get privkeys from files
-const agentpath = "server/bcprivkeys/agent.json";
-const buyerpath = "server/bcprivkeys/buyer.json";
-const sellerpath = "server/bcprivkeys/seller.json";
-const agentkey = JSON.parse(fs.readFileSync(agentpath)).privkey;
-const buyerkey = JSON.parse(fs.readFileSync(buyerpath)).privkey;
-const sellerkey = JSON.parse(fs.readFileSync(sellerpath)).privkey;
-const privKeyBestonChain = [agentkey,buyerkey,sellerkey];
+// const agentpath = "server/bcprivkeys/agent.json";
+// const buyerpath = "server/bcprivkeys/buyer.json";
+// const sellerpath = "server/bcprivkeys/seller.json";
+// const agentkey = JSON.parse(fs.readFileSync(agentpath)).privkey;
+// const buyerkey = JSON.parse(fs.readFileSync(buyerpath)).privkey;
+// const sellerkey = JSON.parse(fs.readFileSync(sellerpath)).privkey;
+// const privKeyBestonChain = [agentkey,buyerkey,sellerkey];
 
 module.exports = {
   networks: {
     bestonchain: {
-      provider: () => new HDWalletProvider(privKeyBestonChain, "http://125.63.52.142:8545"),
+      // provider: () => new HDWalletProvider(privKeyBestonChain, "http://125.63.52.142:8545"), // if connecting to VPS node1 to deploy
+      provider: () => new HDWalletProvider(privKeyBestonChain, "http://127.0.0.1:8545"), // if connecting to Laptop node1 to deploy
       network_id: 100,
       confirmations: 2,
       timeoutBlocks: 200,
