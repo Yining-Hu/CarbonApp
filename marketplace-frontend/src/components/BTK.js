66586a268d52b8ed2986f7291038a99e6cab46b5
Yining Hu <yining.hu@uts.edu.au>
2023-05-09 11:29:28 +1000

    enables Buy BTK on frontend

--- marketplace-frontend/src/components/BTK.js
+++ marketplace-frontend/src/components/BTK.js
@@ -3,7 +3,9 @@ import axios from 'axios';
 
 export default class BTK extends React.Component {
   state = {
-    balances:[]
+    balances:[],
+    amountTobuy:"",
+    amountTosell:""
   }
 
   componentDidMount() {
@@ -21,6 +23,49 @@ export default class BTK extends React.Component {
         this.setState({ balances });
       })
   }
+
+  handleChange = event => {
+    this.setState({
+      ...this.state,
+      [event.target.name]: event.target.value,
+    })
+  }
+
+  handleBuy = event => {
+    event.preventDefault();
+
+    const apiConfig = {
+      headers:{
+          "api-key":"Mt0sYr7p3Kn8Ua8BWPvlURbhCOLmWq",
+          "user-name":"buyer",
+          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'application/json',
+      },
+    }
+
+    axios.post(`http://localhost:3000/marketplace/btk/buy`, {amountTobuy:this.state.amountTobuy,gas:300000}, apiConfig)
+    .then(res => {
+      console.log(res.data);
+    })
+  };
+
+  handleSell = event => {
+    event.preventDefault();
+
+    const apiConfig = {
+      headers:{
+          "api-key":"Mt0sYr7p3Kn8Ua8BWPvlURbhCOLmWq",
+          "user-name":"buyer",
+          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'application/json',
+      },
+    }
+
+    axios.post(`http://localhost:3000/marketplace/btk/sell`, {amountTosell:this.state.amountTosell,gas:300000}, apiConfig)
+    .then(res => {
+      console.log(res.data);
+    })
+  };
 
   render() {
     return (
@@ -43,19 +88,22 @@ export default class BTK extends React.Component {
         </table>
 
         <h2>Buy BTK</h2>
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={this.handleBuy}>
           <div className='form-div'>
-            <label className='token-mint-label'>Amount to Buy:</label>
-            <input type="text" name="amount_to_buy"/>
+            <label className='token-mint-label'>Amount:</label>
+            <input type="text" name="amountTobuy" onChange={this.handleChange}/>
+            <button type="submit">Submit</button>
           </div>
-          <div className='form-div'>
-            <label className='token-mint-label'>To:</label>
-            <input type="text" name="to"/>
-          </div>
-          <button type="submit">Add</button>
         </form>
 
         <h2>Sell BTK</h2>
+        <form onSubmit={this.handleSell}>
+          <div className='form-div'>
+            <label className='token-mint-label'>Amount:</label>
+            <input type="text" name="amountTosell" onChange={this.handleChange}/>
+            <button type="submit">Submit</button>
+          </div>
+        </form>
       </div>
     )
   }
