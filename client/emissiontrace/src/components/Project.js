0e58bc5499eceb6c9887e3f986178a0436ede396
Yining Hu <yining.hu@uts.edu.au>
2023-09-05 11:51:11 +1000

    initial commit for array input on forms

--- client/emissiontrace/src/components/Project.js
+++ client/emissiontrace/src/components/Project.js
@@ -10,7 +10,8 @@ export default class Project extends React.Component {
     baselineend:"",
     projectstart:"",
     projectend:"",
-    herds:"",
+    name:"",
+    herds:[{name:""}],
   }
 
   componentDidMount() {
@@ -27,6 +28,7 @@ export default class Project extends React.Component {
       .then(res => {
         const projects = res.data;
         this.setState({ projects });
+        console.log(projects[2].herds)
       })
   }
 
@@ -37,12 +39,33 @@ export default class Project extends React.Component {
     })
   }
 
-  handleArrayInput = event => {
+  handleHerdNameChange = (idx) => (evt) => {
+    const newHerds = this.state.herds.map((herd, hidx) => {
+      if (idx !== hidx) return herd;
+      return { ...herd, name: evt.target.value };
+    });
+
+    this.setState({ herds: newHerds });
+  };
+
+  handleAddHerd = () => {
     this.setState({
-      ...this.state,
-      [event.target.name]: (event.target.value).split(','),
-    })
-  }
+      herds: this.state.herds.concat([{ name: "" }]),
+    });
+  };
+
+  handleRemoveHerd = (idx) => () => {
+    this.setState({
+      herds: this.state.herds.filter((s, hidx) => idx !== hidx),
+    });
+  };
+
+  // handleArrayInput = event => {
+  //   this.setState({
+  //     ...this.state,
+  //     [event.target.name]: (event.target.value).split(','),
+  //   })
+  // }
 
   handleRegister = event => {
     event.preventDefault();
@@ -63,6 +86,27 @@ export default class Project extends React.Component {
      projectstart:this.state.projectstart,
      projectend:this.state.projectend,
      herds:this.state.herds,
+     gas:300000}, apiConfig)
+    .then(res => {
+      console.log(res.data);
+    })
+  };
+
+  handleUpdate = event => {
+    event.preventDefault();
+
+    const apiConfig = {
+      headers:{
+          "api-key":"dTR4EeuqZgPcW6dAvv1Ka7805Sht6P",
+          "user-name":"beston",
+          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'application/json',
+      },
+    }
+
+    axios.post(`http://localhost:3000/project/add/herds`, 
+    {projectid:this.state.projectid,
+     herdids:this.state.herds,
      gas:300000}, apiConfig)
     .then(res => {
       console.log(res.data);
@@ -91,14 +135,14 @@ export default class Project extends React.Component {
                 <td>{parseTimestamp(project.baselineend)}</td>
                 <td>{parseTimestamp(project.projectstart)}</td>
                 <td>{parseTimestamp(project.projectend)}</td>
-                <td>{project.herds}</td>
+                <td>{(project.herds).toString()}</td>
               </tr>
             )
         }
         </table>
 
         <h2>Register a Carbon Offset Project</h2>
-        <form onSubmit={this.handleIssue}>
+        <form onSubmit={this.handleRegister}>
           <div className='form-div'>
             <label className='cbtoken-label'>ProjectID:</label>
             <input type="text" name="projectid" onChange={this.handleChange}/>
@@ -119,12 +163,40 @@ export default class Project extends React.Component {
             <label className='cbtoken-label'>ProjectID:</label>
             <input type="text" name="projectend" onChange={this.handleChange}/>
           </div>
-          <div className='form-div'>
+          {/* <div className='form-div'>
             <label className='cbtoken-label'>Herds:</label>
             <input type="text" name="herd" onChange={this.handleArrayInput}/>
-          </div>
+          </div> */}
           <button type="submit">Submit</button>
         </form>
+
+        <h2>Add Herds to a Carbon Offset Project</h2>
+        <form onSubmit={this.handleUpdate}>
+          <div className='form-div'>
+            <label className='cbtoken-label'>ProjectID:</label>
+            <input type="text" name="projectid" onChange={this.handleChange}/>
+          </div>
+          <div className='form-div'>
+              {this.state.herds.map((herd, idx) => (
+                <div className='form-div'>
+                  <label className='cbtoken-label'>Herd:</label>
+                  <input
+                    type="text"
+                    placeholder={`Herd #${idx + 1} id`}
+                    value={herd.name}
+                    onChange={this.handleHerdNameChange(idx)}
+                  />
+                  <button type="button" onClick={this.handleAddHerd} className="small">
+                    +
+                  </button>
+                  <button type="button" onClick={this.handleRemoveHerd(idx)} className="small">
+                    -
+                  </button>
+                </div>
+              ))}
+            </div>
+            <button type="submit">Update</button>
+          </form>
       </div>
     )
   }
